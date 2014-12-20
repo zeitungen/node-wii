b832b1c0458466e0ef270613c87651e82f4e1277
Maxime Journaux <journaux.maxime@gmail.com>
2014-12-20 15:48:50 +0100

    add constants into module.exports

--- node-wii.js
+++ node-wii.js
@@ -1,12 +1,20 @@
 // javascript shim that lets our object inherit from EventEmitter
-var WiiMote = require(__dirname + '/build/Release/nodewii.node').WiiMote;
+var nodewii = require(__dirname + '/build/Release/nodewii.node');
+var WiiMote = nodewii.WiiMote;
 var events = require('events');
 
 inherits(WiiMote, events.EventEmitter);
+copy(exports,nodewii);
 exports.WiiMote = WiiMote;
 
 // extend prototype
 function inherits(target, source) {
   for (var k in source.prototype)
     target.prototype[k] = source.prototype[k];
+}
+
+// copy object
+function copy ( target, source ) {
+	for ( var k in source )
+		target[k] = source[k];
 }
